87b776a665e397628771fe76c2dbc23b44bc3d9f
Kyoungjin Lim <klim30@jhu.edu>
2022-12-09 00:51:02 -0500

    [ADD] testing file modified

--- testing.py
+++ testing.py
@@ -8,6 +8,7 @@ from bwa import *
 from proteinToDNA import *
 from proteinIndex import *
 from openRF import *
+from blosum import *
 
 # dataset - 100 protein sequences for E. Coli chosen by name (alphabetical order)
 dataset = 'uniprot-compressed_true_download_true_format_fasta_query_accession_3-2022.12.08-22.52.25.99.fasta'
@@ -42,16 +43,26 @@ protein_sequence = "QIVLARELNLDQIRAIHQATDATIEF"
 # change to DNA
 DNA = proteinToDNA(protein_sequence)
 
+#NA = DNA[0:2] + 'A' + DNA[3:] 
+print(DNA)
+
 # open reading frame
+passed_test = []
+passed_protein = []
 tests = getRF(DNA)
 for test1 in tests:
-    output = bwa(test1, SA, C, O, O_rev, n, alphabet, proteinIDs)
+    output = bwa(test1, SA, C, O, O_rev, n, alphabet, proteinIDs, 1)
     for o in output:
+        passed_test.append(test1)
+        passed_protein.append(proteins[protein_id[o]])
         print("Protein match: %s" % protein_id[o])
 
+print(passed_test)
+print(passed_protein)
 # verify using FM
-protein_index = FMIndex(reference + '$', 20, 5)
-output = protein_index.query(test1)
+protein_index = FMIndex(passed_protein[0] + '$', 10, 5)
+output = protein_index.query(passed_test[0])
 print(output)
 
-
+print(len(passed_test[0]))
+localAlignment(passed_protein[0], passed_test[0], -1, -1,  True)
